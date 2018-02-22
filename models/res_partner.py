a570b01703c262d12f1977e826e98e13501cebf7
Tony Galmiche <tony.galmiche@infosaone.com>
2018-02-22 14:55:17 +0100

    liste des fournisseurs

--- models/res_partner.py
+++ models/res_partner.py
@@ -6,6 +6,7 @@ from odoo import api, fields, models, _
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    is_code_client = fields.Char('Code Client')
+    is_code_client      = fields.Char('Code Client')
+    is_code_fournisseur = fields.Char('Code Fournisseur')
 
 
