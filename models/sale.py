84e3cfea9f05395bf34b408ab2f9beae319e87ee
Tony Galmiche <tony.galmiche@infosaone.com>
2019-09-04 11:10:43 +0200

    convertir devis en commande vente

--- models/sale.py
+++ models/sale.py
@@ -7,4 +7,21 @@ class SaleOrder(models.Model):
 
     is_affaire_id      = fields.Many2one('is.affaire', u'Machine')
     is_delai_livraison = fields.Char(u'Délai de livraison')
+    is_devis_id        = fields.Many2one('sale.order', u"Devis d'origine")
+
+
+    @api.multi
+    def convertir_en_commande(self):
+        for obj in self:
+            copy = obj.copy()
+            copy.is_devis_id = obj.id
+            res= {
+                'name': u'Devis transformé en commande',
+                'view_mode': 'form',
+                'view_type': 'form',
+                'res_model': 'sale.order',
+                'type': 'ir.actions.act_window',
+                'res_id': copy.id,
+            }
+            return res
 
