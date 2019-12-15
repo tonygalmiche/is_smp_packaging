6c07d7db7510353a83ee22b912182cc2e853216d
Tony Galmiche <tony.galmiche@infosaone.com>
2019-12-15 08:27:04 +0100

    modif

--- models/stock_move.py
+++ models/stock_move.py
@@ -6,7 +6,7 @@ class StockMove(models.Model):
     _inherit = "stock.move"
     _order='date desc, id desc'
 
-    is_affaire_id = fields.Many2one('is.affaire', u'Machine', compute='_compute_is_affaire_id', store=True)
+    is_affaire_id = fields.Many2one('is.affaire', u'Machine', compute='_compute_is_affaire_id', store=True, readonly=False)
 
     @api.depends('purchase_line_id.is_affaire_id')
     def _compute_is_affaire_id(self):
