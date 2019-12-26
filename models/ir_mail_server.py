b3bcbef852cafa6b96f9df1562f3af01de1cf220
Tony Galmiche <tony.galmiche@infosaone.com>
2019-12-26 08:50:04 +0100

    bug cc en double

--- models/ir_mail_server.py
+++ models/ir_mail_server.py
@@ -9,10 +9,31 @@ class IrMailServer(models.Model):
     @api.model
     def send_email(self, message, mail_server_id=None, smtp_server=None, smtp_port=None,
                    smtp_user=None, smtp_password=None, smtp_encryption=None, smtp_debug=False):
-        cc=''
-        if message['From']:
-            cc=message['From']
-        message['Cc'] = cc
+
+
+#        print 'Cc=',message['Cc'],message['From']
+
+#        cc=''
+#        if message['From']:
+
+
+#            cc=message['From']
+#        message['Cc'] = cc
+
+#Cc= tony.galmiche@gmail.com Administrator <tony.galmiche@free.fr>
+
+
+
+#        print 'Cc=',message['Cc'],message['From']
+#        print message
+
+#        print '#### type=',type(message)
+
+#        for line in message:
+#            print 'line=',line
+
+
+
 
         res = super(IrMailServer, self).send_email(message, mail_server_id, smtp_server, smtp_port,
                    smtp_user, smtp_password, smtp_encryption, smtp_debug)
--- wizard/mail_compose_message.py
+++ wizard/mail_compose_message.py
@@ -32,12 +32,12 @@ class Partner(models.Model):
         mail_values = super(Partner, self)._notify_prepare_email_values(message)
         cc_email_list = message.is_partner_copie_ids.mapped('email')
 
-#        #** Mettre en copie l'emetteur du mail *********************************
-#        user  = self.env['res.users'].browse(self._uid)
-#        email = user.email
-#        if email:
-#            cc_email_list.append(email)
-#        #***********************************************************************
+        #** Mettre en copie l'emetteur du mail *********************************
+        user  = self.env['res.users'].browse(self._uid)
+        email = user.email
+        if email:
+            cc_email_list.append(email)
+        #***********************************************************************
 
         partner_copie = {
             'email_cc': ",".join(cc_email_list),
