30cae7fa1641f24ab28125543d63aaa4e7d4324c
Tony Galmiche <tony.galmiche@infosaone.com>
2021-05-10 10:47:06 +0200

    modifs

--- models/is_account_invoice_line.py
+++ models/is_account_invoice_line.py
@@ -8,6 +8,13 @@ class is_account_invoice_line(models.Model):
     _auto = False
 
 
+    @api.depends('price_unit')
+    def _compute(self):
+        for obj in self:
+            obj.cout_total = obj.standard_price*obj.quantity
+            obj.marge      = (obj.price_unit-obj.standard_price)*obj.quantity
+
+
     invoice_id             = fields.Many2one('account.invoice', u'Facture')
     number                 = fields.Char(u"N°Facture")
     date_invoice           = fields.Date(u"Date Facture")
@@ -21,6 +28,9 @@ class is_account_invoice_line(models.Model):
     partner_id             = fields.Many2one('res.partner', u'Client/Fournisseur Facturé')
     type                   = fields.Char(u"Type Facture")
     state                  = fields.Char(u"Etat Facture")
+    standard_price         = fields.Float(u'Coût unitaire', related='product_id.standard_price', readonly=True)
+    cout_total             = fields.Float(u'Coût total'  , digits=(14,4), compute='_compute', store=False, readonly=True)
+    marge                  = fields.Float(u'Marge totale', digits=(14,4), compute='_compute', store=False, readonly=True)
 
 
     def init(self):
@@ -43,7 +53,9 @@ class is_account_invoice_line(models.Model):
                     ai.partner_id,
                     ai.type,
                     ai.state
-                from account_invoice_line ail inner join account_invoice ai on ail.invoice_id=ai.id
+                from account_invoice_line ail inner join account_invoice   ai on ail.invoice_id=ai.id
+                                               left join product_product   pp on ail.product_id=pp.id
+                                               inner join product_template pt on pp.product_tmpl_id=pt.id
             );
         """)
 
